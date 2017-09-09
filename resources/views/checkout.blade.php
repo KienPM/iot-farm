6266f07989852f77d4562decc9b485a01924f0c9
HoangHoi <hoanghoi1310@gmail.com>
2017-09-10 04:05:52 +0700

    Add checkout success view

---
+++ resources/views/checkout.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-body" style="min-height: 82vh; font-size: 20px">
+                    <i class="fa fa-check" aria-hidden="true"></i>
+                    Thanh toán thành công!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
