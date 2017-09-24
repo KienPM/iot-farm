234f09d53f12f483e91b7dc954590fadece81ccc
HoangHoi <hoanghoi1310@gmail.com>
2017-09-24 13:40:55 +0700

    Checkout final

---
+++ resources/views/checkout/cart-empty.blade.php
@@ -0,0 +1,6 @@
+@extends('checkout.master')
+
+@section('message')
+    <i class="fa fa-times" aria-hidden="true" style="color: red;"></i>
+    <span style="color: black;">Giỏ hàng rỗng!</span>
+@endsection
